547d3f8baf4e3a4fe412139ad333a5636362c9be
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-10-20 17:26:46 +0200

    feat: created mockup layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import type { Component } from "solid-js";
 import { css } from "solid-styled";
+import CodeWindow from "./CodeWindow";
 
 const App: Component = () => {
   css`
+    nav {
+      padding: var(--size-5);
+    }
+
     main {
-      background-color: red;
+      display: flex;
+      justify-content: center;
+    }
+    
+    footer {
+      padding: var(--size-5);
+      margin-top: auto;
+      display: flex;
+      justify-content: center;
+    }
+
+    footer p {
+      font-size: var(--font-size-0);
     }
   `;
 
   return (
-    <main>
-      using solid-styled
-    </main>
+    <>
+      <nav>JavaScript Obfuscator</nav>
+      <main>
+        <CodeWindow/>
+      </main>
+      <footer>
+        <p>© 2022 Piotr Krzystanek and Bartłomiej Chmiel</p>
+      </footer>
+    </>
   );
 };
 
