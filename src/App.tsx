b4c79194c95d89b55a090ea511b7da57f43ae0f8
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-10-24 11:31:34 +0200

    feat: created transform

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import type { Component } from 'solid-js';
 
 import styles from './App.module.css'
+import { createEffect, createSignal } from "solid-js";
 
 const App: Component = () => {
+  const [getInput, setInput] = createSignal("");
+  const [getOutput, setOutput] = createSignal("");
+
+  const transform = (input: string) => {
+    return input;
+  }
+
+  createEffect(() => {
+    const input = getInput();
+    const output = transform(input);
+    setOutput(output);
+  })
+
   return (
     <main class={styles.main}>
-      <textarea class={styles.textarea}>area 1</textarea>
-      <textarea class={styles.textarea}>area 2</textarea>
+      <textarea class={styles.textarea} onInput={input => setInput((input.target as HTMLInputElement).value)} value={getInput()}/>
+      <textarea class={styles.textarea} readonly value={getOutput()} />
     </main>
   );
 };
