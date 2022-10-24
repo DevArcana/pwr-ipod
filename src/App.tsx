2588ec4199d2f3551b77d9bb88833605c74f7884
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-10-24 11:36:57 +0200

    feat: added error resilience

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import type { Component } from 'solid-js';
-import {Parser} from 'acorn';
-
-import styles from './App.module.css'
+import type { Component } from "solid-js";
 import { createEffect, createSignal } from "solid-js";
+import { Parser } from "acorn";
+
+import styles from "./App.module.css";
 
 const App: Component = () => {
-  const [getInput, setInput] = createSignal("");
-  const [getOutput, setOutput] = createSignal("");
+  const [ getInput, setInput ] = createSignal("");
+  const [ getOutput, setOutput ] = createSignal("");
 
   const transform = (input: string) => {
-    return JSON.stringify(Parser.parse(input, {ecmaVersion: 2020}))
-  }
+    try {
+      return JSON.stringify(Parser.parse(input, { ecmaVersion: 2020 }));
+    } catch (e) {
+      return JSON.stringify(e);
+    }
+  };
 
   createEffect(() => {
     const input = getInput();
     const output = transform(input);
     setOutput(output);
-  })
+  });
 
   return (
     <main class={styles.main}>
-      <textarea class={styles.textarea} onInput={input => setInput((input.target as HTMLInputElement).value)} value={getInput()}/>
-      <textarea class={styles.textarea} readonly value={getOutput()} />
+      <textarea class={styles.textarea} onInput={input => setInput((input.target as HTMLInputElement).value)}
+                value={getInput()}/>
+      <textarea class={styles.textarea} readonly value={getOutput()}/>
     </main>
   );
 };
