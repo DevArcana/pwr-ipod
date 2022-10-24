7d0db42e8d878e2aaf8a2dd5c2b515ae2d08883b
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-10-24 11:52:49 +0200

    feat: added option to display ast

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,67 @@
 import type { Component } from "solid-js";
-import { createEffect, createSignal } from "solid-js";
+import { batch, createEffect, createSignal } from "solid-js";
 import { Parser } from "acorn";
 
 import styles from "./App.module.css";
+import { generate } from "astring";
 
 const App: Component = () => {
-  const [ getInput, setInput ] = createSignal("");
-  const [ getOutput, setOutput ] = createSignal("");
+  const options = { ecmaVersion: 2020 };
 
-  const transform = (input: string) => {
-    try {
-      return JSON.stringify(Parser.parse(input, { ecmaVersion: 2020 }));
-    } catch (e) {
-      return JSON.stringify(e);
-    }
-  };
+  const [ getInput, setInput ] = createSignal("");
+  const [ getAst, setAst ] = createSignal(Parser.parse(getInput(), { ecmaVersion: 2020 }));
+  const [ getOutput, setOutput ] = createSignal("");
+  const [ getException, setException ] = createSignal<string>();
+
+  const [ getDisplayAst, setDisplayAst ] = createSignal(false);
 
   createEffect(() => {
     const input = getInput();
-    const output = transform(input);
+
+    try {
+      const ast = Parser.parse(getInput(), { ecmaVersion: 2020 });
+      batch(() => {
+        setAst(ast);
+        setException(undefined);
+      });
+    } catch (e) {
+      setException(JSON.stringify(e));
+    }
+  });
+
+  createEffect(() => {
+    const ast = getAst();
+    const output = generate(ast);
     setOutput(output);
   });
 
+  const resolveOutput = () => {
+    const exception = getException();
+    if (exception)
+      return exception;
+
+    const output = getDisplayAst() ? JSON.stringify(getAst()) : getOutput();
+    return output;
+  };
+
+  const onInput = (event: InputEvent) => {
+    const target = event.target as HTMLInputElement;
+    setInput(target.value);
+  };
+
+  const onAstDisplayInput = (event: MouseEvent) => {
+    const target = event.target as HTMLInputElement;
+    setDisplayAst(!getDisplayAst());
+  };
+
   return (
     <main class={styles.main}>
-      <textarea class={styles.textarea} onInput={input => setInput((input.target as HTMLInputElement).value)}
-                value={getInput()}/>
-      <textarea class={styles.textarea} readonly value={getOutput()}/>
+      <nav class={styles.nav}>
+        <input type="checkbox" name="displayAst" checked={getDisplayAst()} onClick={onAstDisplayInput}/>
+        <label for="displayAst">display AST</label>
+      </nav>
+      <textarea class={styles.textarea} onInput={onInput} value={getInput()}/>
+      <textarea class={styles.textarea} readOnly value={resolveOutput()}/>
     </main>
   );
 };
