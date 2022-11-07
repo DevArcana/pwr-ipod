c565d79454df21137912bf8e4b28f3abf1a1526a
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-11-07 11:36:37 +0100

    added codemirrors

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import CodeMirror from '@uiw/react-codemirror';
+// @ts-ignore
+import { javascript } from "@codemirror/lang-javascript";
+import "./App.css";
+import { Parser } from "acorn";
+import { generate } from "astring";
 
 function App() {
+  const options = { ecmaVersion: 2020 };
+  const [ input, setInput ] = useState("");
+  const [ ast, setAst ] = useState(Parser.parse("", { ecmaVersion: 2020 }));
+  const [ output, setOutput ] = useState("");
+  const [ exception, setException ] = useState<string>();
+  const [ displayAst, setDisplayAst ] = useState(false);
+  const [ resolved, setResolved ] = useState("");
+
+  useEffect(() => {
+    const transformed = input;
+
+    try {
+      const ast = Parser.parse(transformed, { ecmaVersion: 2020 });
+      setAst(ast);
+      setException(undefined);
+    } catch (e) {
+      setException(JSON.stringify(e));
+    }
+  }, [input]);
+
+  useEffect(() => {
+    const transformedAst = ast;
+    const transformedOutput = generate(transformedAst);
+    setOutput(transformedOutput);
+  }, [ast]);
+
+  useEffect(() => {
+    if (exception) {
+      setResolved(exception)
+    }
+    else {
+      setResolved(displayAst ? JSON.stringify(ast) : output)
+    }
+  }, [output, exception, displayAst])
+
+  const onChange = React.useCallback((value: string, viewUpdate: any) => {
+    setInput(value);
+  }, []);
+
+  const onDisplayAstChange = () => {
+    setDisplayAst(!displayAst);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <nav className="nav">
+        <input type="checkbox" name="displayAst" checked={displayAst} onClick={onDisplayAstChange}/>
+        <label htmlFor="displayAst">display AST</label>
+      </nav>
+      <CodeMirror
+        className="textarea"
+        value={input}
+        height="100%"
+        width="100%"
+        extensions={[javascript({ jsx: false })]}
+        onChange={onChange}
+      />
+      <CodeMirror
+        className="textarea"
+        editable={false}
+        value={resolved}
+        height="100%"
+        width="100%"
+        extensions={[javascript({ jsx: false })]}
+        basicSetup={{highlightActiveLine: false, highlightActiveLineGutter: false}}
+      />
+    </main>
   );
 }
 
