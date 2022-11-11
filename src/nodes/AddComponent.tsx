be60a39f17912fd57daff32f1abb0c4f362dcadf
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-11-11 21:02:19 +0100

    feat: split all components into separate files

---
+++ src/nodes/AddComponent.tsx
@@ -0,0 +1,114 @@
+import Rete from "rete";
+import { numSocket } from "../sockets";
+import { NumControl } from "../controls/NumControl";
+import React from "react";
+// @ts-ignore
+import { Control, Node, Socket } from "rete-react-render-plugin";
+
+class Component extends Node {
+  private props: any;
+  private state!: {
+    outputs: any[]
+    controls: any[]
+    inputs: any[]
+    selected: boolean
+  };
+
+  render() {
+    const { node, bindSocket, bindControl } = this.props;
+    const { outputs, controls, inputs, selected } = this.state;
+
+    return (
+      <div className={`node ${selected}`}>
+        <div className="title">
+          {node.name}
+        </div>
+        {/* Outputs */}
+        {outputs.map((output) => (
+          <div className="output" key={output.key}>
+            <div className="output-title">{output.name}</div>
+            <Socket
+              type="output"
+              socket={output.socket}
+              io={output}
+              innerRef={bindSocket}
+            />
+          </div>
+        ))}
+        {/* Controls */}
+        {controls.map((control) => (
+          <Control
+            className="control"
+            key={control.key}
+            control={control}
+            innerRef={bindControl}
+          />
+        ))}
+        {/* Inputs */}
+        {inputs.map((input) => (
+          <div className="input" key={input.key}>
+            <Socket
+              type="input"
+              socket={input.socket}
+              io={input}
+              innerRef={bindSocket}
+            />
+            {!input.showControl() && (
+              <div className="input-title">{input.name}</div>
+            )}
+            {input.showControl() && (
+              <Control
+                className="input-control"
+                control={input.control}
+                innerRef={bindControl}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export class AddComponent extends Rete.Component {
+  constructor() {
+    super("Add");
+
+    // @ts-ignore
+    this.data.component = Component;
+  }
+
+  // @ts-ignore
+  builder(node) {
+    const inp1 = new Rete.Input("num1", "Number", numSocket);
+    const inp2 = new Rete.Input("num2", "Number2", numSocket);
+    const out = new Rete.Output("num", "Number", numSocket);
+
+    inp1.addControl(new NumControl(this.editor, "num1", node));
+    inp2.addControl(new NumControl(this.editor, "num2", node));
+
+    return node
+      .addInput(inp1)
+      .addInput(inp2)
+      .addControl(new NumControl(this.editor, "preview", node, true))
+      .addOutput(out);
+  }
+
+  // @ts-ignore
+  worker(node, inputs, outputs) {
+    const n1 = inputs["num1"].length ? inputs["num1"][0] : node.data.num1;
+    const n2 = inputs["num2"].length ? inputs["num2"][0] : node.data.num2;
+    const sum = n1 + n2;
+
+    const editor = this.editor!;
+
+    const preview = editor.nodes
+      .find((n) => n.id == node.id)!
+      .controls.get("preview")!;
+
+    // @ts-ignore
+    preview.setValue(sum);
+
+    outputs["num"] = sum;
+  }
+}
--- src/rete.jsx
+++ src/rete.jsx
@@ -5,45 +5,10 @@ import ReactRenderPlugin from "rete-react-render-plugin";
 import ConnectionPlugin from "rete-connection-plugin";
 import AreaPlugin from "rete-area-plugin";
 import Context from "efficy-rete-context-menu-plugin";
-import {MyNode} from "./MyNode";
-import {NumControl} from "./controls/NumControl";
-import {numSocket} from "./sockets";
+
 import {NumComponent} from "./nodes/NumComponent";
 import {TextComponent} from "./nodes/TextComponent";
-
-class AddComponent extends Rete.Component {
-    constructor() {
-        super("Add");
-        this.data.component = MyNode; // optional
-    }
-
-    builder(node) {
-        const inp1 = new Rete.Input("num1", "Number", numSocket);
-        const inp2 = new Rete.Input("num2", "Number2", numSocket);
-        const out = new Rete.Output("num", "Number", numSocket);
-
-        inp1.addControl(new NumControl(this.editor, "num1", node));
-        inp2.addControl(new NumControl(this.editor, "num2", node));
-
-        return node
-            .addInput(inp1)
-            .addInput(inp2)
-            .addControl(new NumControl(this.editor, "preview", node, true))
-            .addOutput(out);
-    }
-
-    worker(node, inputs, outputs) {
-        const n1 = inputs["num1"].length ? inputs["num1"][0] : node.data.num1;
-        const n2 = inputs["num2"].length ? inputs["num2"][0] : node.data.num2;
-        const sum = n1 + n2;
-
-        this.editor.nodes
-            .find((n) => n.id == node.id)
-            .controls.get("preview")
-            .setValue(sum);
-        outputs["num"] = sum;
-    }
-}
+import {AddComponent} from "./nodes/AddComponent";
 
 export async function createEditor(container) {
     const components = [new NumComponent(), new AddComponent(), new TextComponent()];
