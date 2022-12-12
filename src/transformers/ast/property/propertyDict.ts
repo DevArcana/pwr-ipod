035efdc634a5d78aae915101c09e9b1f7c098797
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-12-12 10:16:44 +0100

    feat: unproperty to dict

--- src/transformers/ast/property/propertyDict.ts
+++ src/transformers/ast/property/propertyDict.ts
@@ -27,16 +27,12 @@ export default abstract class PropertyDict {
     full(data, (node: acorn.Node) => {
       if (node.type === NodeTypes.MemberExpression) {
         const exp = node as unknown as acornTypes.MemberExpression;
-        const name = (exp.property as acornTypes.Identifier).name;
+
+        exp.property.type = "Identifier";
         // @ts-ignore
-        delete exp.property["name"];
+        exp.property["name"] = (exp.property as acornTypes.Literal).value
 
-        exp.property.type = "Literal";
-
-        (exp.property as acornTypes.Literal).value = name;
-        (exp.property as acornTypes.Literal).raw = `"${name}"`;
-
-        exp.computed = true;
+        exp.computed = false;
       }
     });
 
